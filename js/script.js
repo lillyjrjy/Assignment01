0f2505670edafc764a1066215fe5f8f1aec0a66e
lillyjrjy <56412294+lillyjrjy@users.noreply.github.com>
2023-09-23 18:36:28 -0700

    first 6 questions

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// STEP 1
+/*
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+// STEP 2
+   //numeric literal expression: 10
+   //string literal expression: "Assignment 01"
+   //Boolean literal expression: true
+   //null literal expression: null
+// STEP 3
+    //let compExp = 2 * 63
+    //let anothercompExp = compExp % 7
+// STEP 4 
+    // let strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, blnMayWeContactYou
+/* STEP 5
+    
+    let strFirstName, strCity, strState
+    strFirstName = 'Chunhui Yang'
+    strCity = 'San Diego'
+    strState = 'CA'
+  //------------ 
+    let strFirstName =  'Chunhui Yang', strCity = 'San Diego', strState = 'CA'
+  //------------
+  let strFirstName =  'Chunhui Yang'
+  let strCity = 'San Diego'
+  let strState = 'CA' 
+*/
+    
+/* STEP 6
+   let intNum = 1
+   document.write(`<h1>This is ${intNum} solid sentence <\h1>`)
+*/
+// STEP 7
+// STEP 3
+// STEP 3
+// STEP 3
+// STEP 3
+// STEP 3
+// STEP 3
+
