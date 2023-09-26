73294cc539aef2bd6542c5dd9c3e2cdb9ae131af
lillyjrjy <56412294+lillyjrjy@users.noreply.github.com>
2023-09-25 20:50:41 -0700

    done
    thing tis done

--- js/script.js
+++ js/script.js
@@ -32,19 +32,65 @@ let myLibraryFunction
     
 /* STEP 6
    let intNum = 1
-   document.write(`<h1>This is ${intNum} solid sentence <\h1>`)
+   alert('This is ' + intNum + ' solid sentence' )
 */
 // STEP 7
-  //  let blnOkay = true
-  //  document.write(`it's ${blnOkay} that this is a sentence<br/>`)
+     //   let blnOkay = true
+     //   alert('it\'s ' +blnOkay+ ' that this is a sentence')
+     //   let intNumm = 2
+     //   alert(blnOkay + intNumm)
 
 // STEP 8
-    //it's not, I would fix it:
+    //it's not, I would fix:
    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
    // console.log(someString)
-// STEP 3
-// STEP 3
-// STEP 3
-// STEP 3
-// STEP 3
+// STEP 9
+    // let x=null
+    // let y
+    // console.log(x)   
+    // console.log(y)   
 
+// STEP 10
+        // console.log(typeof('string'))
+        // console.log(typeof(1))
+        // console.log(typeof(true))
+        // console.log(typeof({x:1, y:2}))
+        // let x
+        // console.log(typeof(x))
+// STEP 11
+    //alert('Hello'+' Lilly Yang' +', welcome to the JavaScript class!')
+// STEP 12
+//     let name = 'Lilly Yang'
+//     alert('Hello '+ name +', welcome to the JavaScript class!')
+// STEP 13
+    // let course = 'JavaScript'
+    // alert(`Hello Lilly Yang, welcome to the ${course} class!`)     
+// STEP 14
+    //alert(`Hello Lilly Yang.\nWelcome to the JavaScript class!`)
+// STEP 15
+    // let name = prompt('what\'s your name?')
+    // alert(`Hello ${name}, welcome to the JavaScript class!`)
+// STEP 16
+    // let course = prompt('what\'s your course?')
+    // alert(`Hello Lilly Yang, welcome to the ${course} class!`)
+// STEP 17
+    // let x = 10
+    // let y = 20
+    // console.log(x+y)
+// STEP 18
+    // let x = 20
+    // x+=x
+    // console.log(x)
+// STEP 19
+    // let x = 20
+    // x = x*5
+    // console.log(x)
+// STEP 20
+    // let x = 20%3
+    // x = x/1
+    // console.log(x)
+
+// STEP 21
+    //console.log(1==1)
+// STEP 22
+    //console.log(1>2)
